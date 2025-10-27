417f8fec40f439ae39e09b86f75e1e190bf0ffe1
Vezyth <juliuskevinr12@gmail.com>
2025-10-27 14:18:32 +0700

    added hero, video, featuire, workflow, pricing section

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
-import { useState } from 'react'
-import Navbar from './components/Navbar'
+import { useState } from "react";
+import Navbar from "./components/Navbar";
+import HeroSection from "./components/HeroSection";
+import FeatureSection from "./components/FeatureSection";
+import WorkFlow from "./components/WorkFlow";
+import PricingSection from "./components/PricingSection";
 
 
 function App() {
-
   return (
     <>
-    <Navbar/>
-     
+      <Navbar />
+      <div className="max-w-7xl mx-auto pt-20 px-10">
+        <HeroSection />
+        <FeatureSection />
+        <WorkFlow/>
+        <PricingSection/>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
